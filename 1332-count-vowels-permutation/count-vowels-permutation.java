cb837ae132efdee4a73decfb03f792dc342c9aa9
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2023-10-28 17:37:37 +0530

    Time: 5 ms (99.01%) | Memory: 40 MB (77.89%) - LeetSync

---
+++ 1332-count-vowels-permutation/count-vowels-permutation.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int countVowelPermutation(int n) {
+       int mod=1000000007;
+        long a=1,e=1,o=1,i=1,u=1;
+        
+        for(int j=1;j<n;j++){
+            
+            long a_next=e;
+            long e_next=(a+i)%mod;
+            long i_next=(a+e+o+u)%mod;
+            long o_next=(i+u)%mod;
+            long u_next=(a);
+            
+            a=a_next;
+            e=e_next;
+            i=i_next;
+            o=o_next;
+            u=u_next;
+        }
+        return (int)((a+e+i+o+u)%mod);
+    }
+}
+
+
+
