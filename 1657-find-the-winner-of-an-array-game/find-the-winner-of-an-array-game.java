dce509589e62ee26e80ab1f57c4723ec33568ed1
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2023-11-05 19:35:13 +0530

    Time: 0 ms (100.00%) | Memory: 55.8 MB (92.75%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.java
@@ -2,7 +2,7 @@ class Solution {
     public int getWinner(int[] arr, int k) {
       int maxE=arr[0];
       int n=arr.length;
-      int maxL=0;
+      
         int cL=0;
       for(int i=1;i<n;i++){
          if(maxE>arr[i]){
