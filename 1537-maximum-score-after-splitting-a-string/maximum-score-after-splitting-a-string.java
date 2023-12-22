7f6136f9987310f943071ae68503f69789133a99
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2023-12-22 18:13:14 +0530

    Time: 1 ms (97.78%) | Memory: 41.5 MB (32.89%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.java
@@ -1,28 +1,18 @@
 class Solution {
     public int maxScore(String s) {
-        int n=s.length();
-        if(n==2){
-            if(s.equals("00") || s.equals("11")) return 1;
-            else if(s.equals("01")) return 2;
-            else return 0;
-        }
-        int pl[]=new int[n+1];
-        int pr[]=new int[n+1];
-        pr[n]=0;
-        if(s.charAt(0)=='0') pl[0]=1;
-        for(int i=1;i<n;i++){
-            pl[i]=pl[i]+pl[i-1];
-            if(s.charAt(i)=='0') pl[i]++;
-        }
+       int n=s.length();
+       int ones=0;
+       for(int i=0;i<n;i++){
+           if(s.charAt(i)=='1') ones++;
+       }
 
-        for(int i=n-1;i>=0;i--){
-            pr[i]=pr[i]+pr[i+1];
-            if(s.charAt(i)=='1') pr[i]++;
-        }
-        int max=0;
-        for(int i=1;i<n-1;i++){
-            max=Math.max(max,pl[i]+pr[i]);
-        }
-        return max;
+       int zeroes=0;
+       int ans=0;
+       for(int i=0;i<n-1;i++){
+           if(s.charAt(i)=='1') ones--;
+           else zeroes++;
+           ans=Math.max(ans,ones+zeroes);
+       }
+       return ans;
     }
 }
