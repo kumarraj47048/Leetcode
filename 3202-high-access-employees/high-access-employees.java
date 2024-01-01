c68a3319b90609a9097919f81615ed49003b2090
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-01-01 19:28:17 +0530

    Time: 8 ms (99.01%) | Memory: 45.8 MB (5.61%) - LeetSync

--- 3202-high-access-employees/high-access-employees.java
+++ 3202-high-access-employees/high-access-employees.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<String> findHighAccessEmployees(List<List<String>> access_times) {
-       List<String> ans=new ArrayList<>();
-       HashMap<String,List<Integer>> map=new HashMap<>();
-       for(int i=0;i<access_times.size();i++){
-           String empName=access_times.get(i).get(0);
-           if(!map.containsKey(empName)){
-               map.put(empName,new ArrayList<Integer>());
-           }
-           String time=access_times.get(i).get(1);
-           char ch[]=time.toCharArray();
-           int h=(ch[0]-'0')*10 + (ch[1]-'0');
-           int m=(ch[2]-'0')*10 + (ch[3]-'0');
-           map.get(empName).add(h*60+ m);
-       }
-        
-        for(Map.Entry<String,List<Integer>> entry : map.entrySet()){
-            String temp=entry.getKey();
-            List<Integer> list=new ArrayList<>();
-            list=entry.getValue();
-            Collections.sort(list);
-            for(int i=0;i<list.size()-2;i++){
-                if(list.get(i+2)-list.get(i)<=59){
-                    ans.add(temp);
+        Map<String,List<Integer>> map = new HashMap<>();
+        for(List<String> a:access_times)
+        {
+            if(!map.containsKey(a.get(0))){
+                map.put(a.get(0), new ArrayList<>());
+            }
+            char[] s = a.get(1).toCharArray();
+            int h = (s[0]-'0')*10 + s[1]-'0';
+            int m = (s[2]-'0')*10 + s[3]-'0';
+            map.get(a.get(0)).add(h*60+m);
+        }
+        List<String> ans = new ArrayList<>();
+        for(String key: map.keySet())
+        {
+            List<Integer> val = map.get(key);
+            Collections.sort(val);
+            for(int i=0;i<val.size()-2;i++)
+            {
+                if(val.get(i+2)-val.get(i)<=59)
+                {
+                    ans.add(key);
                     break;
                 }
             }
