3402bd8bad3c167d6ed2b6b7bd025a5196c516b4
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-01-09 02:42:17 +0530

    Time: 5 ms (11.14%) | Memory: 45.3 MB (26.19%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,25 +3,17 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int dp[][]=new int[m][n];
-       // Arrays.fill(dp,Integer.MAX_VALUE);
-       for(int i=0;i<m;i++){
-           for(int j=0;j<n;j++){
-               dp[i][j]=-1;
-           }
-       }
-    int ans=func(0,0,grid,dp);
-    return ans;
-    }
+        dp[m-1][n-1]=grid[m-1][n-1];
 
-    public static int func(int i, int j, int grid[][], int dp[][]){
-        int m=grid.length;
-        int n=grid[0].length;
-        if(i>=m || j>=n) return Integer.MAX_VALUE;
-         if(i==m-1 && j==n-1) return grid[i][j];
-          if(dp[i][j]!=-1) return dp[i][j];
-       
-      
-        dp[i][j]=grid[i][j]+ Math.min(func(i+1,j,grid,dp), func(i,j+1,grid,dp));
-        return dp[i][j];
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i<m-1 && j<n-1){
+                    dp[i][j]=Math.min(dp[i+1][j], dp[i][j+1])+grid[i][j];
+                }
+                 if(i>=m-1 && j<n-1) dp[i][j]=grid[i][j]+dp[i][j+1];
+                 if(j>=n-1 && i<m-1) dp[i][j]=grid[i][j]+dp[i+1][j];
+            }
+        }
+        return dp[0][0];
     }
 }
