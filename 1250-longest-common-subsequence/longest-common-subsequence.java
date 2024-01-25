48767d854a70ef14e95825de0ba8cdd56cd85461
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-01-25 09:30:43 +0530

    Time: 26 ms (33.71%) | Memory: 50.5 MB (53.76%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -11,9 +11,9 @@ class Solution {
         if(n1==0 || n2==0) return 0;
         if(dp[n1-1][n2-1]!=-1) return dp[n1-1][n2-1];
         if(text1.charAt(n1-1)==text2.charAt(n2-1)) {
-            dp[n1-1][n2-1]= 1+func(text1,text2,n1-1,n2-1,dp);
+           return  dp[n1-1][n2-1]= 1+func(text1,text2,n1-1,n2-1,dp);
         }
-        else dp[n1-1][n2-1]= Math.max(func(text1,text2,n1-1,n2,dp), func(text1,text2,n1,n2-1,dp));
-        return dp[n1-1][n2-1];
+        else return dp[n1-1][n2-1]= Math.max(func(text1,text2,n1-1,n2,dp), func(text1,text2,n1,n2-1,dp));
+       // return dp[n1-1][n2-1];
     }
 }
