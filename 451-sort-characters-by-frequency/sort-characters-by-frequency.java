287d102bd6ed35d116f3afc4d75826af35bcf1ff
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-02-07 21:06:28 +0530

    Time: 5 ms (94.72%) | Memory: 44.4 MB (93.79%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -2,22 +2,24 @@ class Solution {
     public String frequencySort(String s) {
         int n=s.length();
         int ct[][]=new int[128][2];
+        char ch[]=new char[n];
+        ch=s.toCharArray();
         for(int i=0;i<n;i++){
-           char c=s.charAt(i);
+           char c=ch[i];
            ct[c][0]=(int)(c);
            ct[c][1]++;
         }
         Arrays.sort(ct,(a,b)->(b[1]-a[1]));
-        String sb=new String("");
+        StringBuilder sb=new StringBuilder("");
         for(int i=0;i<128;i++){
             
             if(ct[i][1]>0){
                 int f=ct[i][1];
                 for(int k=0;k<f;k++){
-                    sb+=(char)(ct[i][0]);
+                    sb.append((char)(ct[i][0]));
                 }
             }
         }
-        return sb;
+        return sb.toString();
     }
 }
