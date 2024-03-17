a52f554b89d5637eb05f475e86c1e2443fdc39d3
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-03-18 00:10:52 +0530

    Time: 1 ms (98.87%) | Memory: 44.8 MB (59.12%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,46 +1,29 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        // If the intervals vector is empty, return a vector containing the newInterval
-        if (intervals.length == 0) {
-            return new int[][] {newInterval};
+        int n = intervals.length, i = 0;
+        List<int[]> res = new ArrayList<>();
+
+        // Case 1: No overlapping before merging intervals
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
 
-        int n = intervals.length;
-        int target = newInterval[0];
-        int left = 0, right = n - 1;
+        // Case 2: Overlapping and merging intervals
+        while (i < n && newInterval[1] >= intervals[i][0]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
 
-        // Binary search to find the position to insert newInterval
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (intervals[mid][0] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        // Case 3: No overlapping after merging newInterval
+        while (i < n) {
+            res.add(intervals[i]);
+            i++;
         }
 
-        // Insert newInterval at the found position
-        List<int[]> result = new ArrayList<>();
-        for (int i = 0; i < left; i++) {
-            result.add(intervals[i]);
-        }
-        result.add(newInterval);
-        for (int i = left; i < n; i++) {
-            result.add(intervals[i]);
-        }
-
-        // Merge overlapping intervals
-        List<int[]> merged = new ArrayList<>();
-        for (int[] interval : result) {
-            // If res is empty or there is no overlap, add the interval to the result
-            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
-                merged.add(interval);
-            // If there is an overlap, merge the intervals by updating the end of the last interval in res
-            } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
-            }
-        }
-
-        return merged.toArray(new int[0][]);
+        // Convert List to array
+        return res.toArray(new int[res.size()][]);
     }
 }
