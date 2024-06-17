dff513edcf645f6ebff88bc935c45daba0b07337
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-06-17 11:17:44 +0530

    Time: 60 ms (5.64%) | Memory: 54.2 MB (5.18%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
        HashMap<Integer,Integer> map=new HashMap<>();
-       map.put(0,1);
-       for(int i=1;i<=(int)Math.sqrt(c);i++) map.put(i*i,1);
+       for(int i=0;i<=(int)Math.sqrt(c);i++) map.put(i*i,1);
        for(int i=0;i<=(int)Math.sqrt(c);i++){
         if(map.containsKey(c-(i*i))) return true;
        } 
