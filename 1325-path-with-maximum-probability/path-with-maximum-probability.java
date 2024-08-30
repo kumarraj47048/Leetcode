aad18e7121f075de43d080fffca6b8ee30ff2be5
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-08-30 15:25:28 +0530

    Time: 25 ms (54.91%) | Memory: 56 MB (80.91%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,48 +1,46 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        List<Pair> graph[]=new ArrayList[n];
-        for(int i=0;i<n;i++){
-            graph[i]=new ArrayList<>();
+       
+        List<List<pair>> graph=new ArrayList<>(n);
+        for(int i=0;i<n;i++) graph.add(new ArrayList<>());
+        for(int i=0;i<edges.length;i++)
+        {
+        int x=edges[i][0];
+          int y=edges[i][1];
+          graph.get(x).add(new pair(y,succProb[i]));
+          graph.get(y).add(new pair(x,succProb[i]));
         }
         double dis[]=new double[n];
-        for(int i=0;i<edges.length;i++){
-              int x=edges[i][0];
-              int y=edges[i][1];
-              double prob=succProb[i];
-              graph[x].add(new Pair(y,prob));
-              graph[y].add(new Pair(x,prob));
-        }
-        Queue<Pair> q=new LinkedList<>();
-        for(Pair p:graph[start_node]){
-            int source=p.b;
-            double val=p.val;
-            dis[source]=val;
-            q.add(new Pair(source,val));
-        }
-        
-        
-       // q.add(new Pair(start_node,0));
-        while(q.size()>0){
-            Pair curr=q.poll();
-            int source=curr.b;
-            double valP=dis[source];
-            for(Pair p:graph[source]){
-                int des=p.b;
-                double weight=p.val;
-                if(valP*weight>dis[des]){
-                    dis[des]=valP*weight;
-                    q.add(new Pair(des,dis[des]));
+        Queue<pair> que=new LinkedList<>();
+        Arrays.fill(dis,Double.MIN_VALUE);
+        que.add(new pair(start_node,1));
+        dis[start_node]=1;
+        while(!que.isEmpty())
+        {
+            pair cur=que.poll();
+            int val=cur.val;
+            double w=cur.w;
+            for(pair num:graph.get(val))
+            {
+                int v1=num.val;
+                double w1=num.w;
+                if(dis[v1]<dis[val]*w1)
+                {
+                    dis[v1]=dis[val]*w1;
+                    que.add(new pair(v1,dis[v1]));
                 }
             }
         }
-    return dis[end_node];
+        return dis[end_node];
+
     }
 }
-class Pair{
-    int b;
-    double val;
-    Pair(int b, double val){
-        this.b=b;
+class pair{
+    int val;
+    double w;
+    pair(int val,double w)
+    {
         this.val=val;
+        this.w=w;
     }
 }
