398f9d0c29169d79fea85359de78d5d199449c1b
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-11-25 22:36:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (79.52%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,12 +15,14 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
+       
         List<Integer> list=new ArrayList<>();
+         if(root==null) return list;
         func(root,list,1);
         return list;
     }
     public static void func(TreeNode root, List<Integer> list, int level){
-       if(root==null) return;
+        if(root==null) return;
         if(list.size()<level) list.add(root.val);
         if(root.right!=null) func(root.right,list,level+1);
         if(root.left!=null) func(root.left,list,level+1);
