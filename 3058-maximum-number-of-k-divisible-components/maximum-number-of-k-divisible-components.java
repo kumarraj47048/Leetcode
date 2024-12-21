89c0dc3706f4e9c11a7af88d0a851fccde956066
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2024-12-21 20:19:55 +0530

    Time: 24 ms (81.82%) | Memory: 62.4 MB (55.84%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.java
@@ -1,30 +1,31 @@
-import java.util.ArrayList;
-
 class Solution {
+    //public static int ct=0;
     public int maxKDivisibleComponents(int n, int[][] edges, int[] values, int k) {
-        ArrayList<Integer>[] list = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) list[i] = new ArrayList<>();
-        for (int[] edge : edges) {
-            list[edge[1]].add(edge[0]);
-            list[edge[0]].add(edge[1]);
+        ArrayList<Integer> list[]=new ArrayList[n+1];
+        for(int i=0;i<=n;i++) list[i]=new ArrayList<>();
+        for(int i=0;i<n;i++) values[i]%=k;
+        for(int i[]:edges){
+              list[i[1]].add(i[0]);
+              list[i[0]].add(i[1]);
         }
-        int[] dp = new int[n + 1];
-        int[] ct = new int[1]; // Single-element array to hold the count
-        dfs(list, 0, dp, values, -1, k, ct);
-        return ct[0] + 1; // Include the root itself as a valid component
+        int dp[]=new int[n+1];
+        int ct[]=new int[1];
+       dfs(list,0,dp,values,-1,k,ct);
+       return ct[0]+1;
     }
 
-    private void dfs(ArrayList<Integer>[] list, int root, int[] dp, int[] values, int parent, int k, int[] ct) {
-        dp[root] = values[root] % k; // Take modulo of the current node's value
-        for (int neighbor : list[root]) {
-            if (neighbor != parent) {
-                dfs(list, neighbor, dp, values, root, k, ct);
-                if (dp[neighbor] == 0) {
-                    ct[0]++; // Increment count if child subtree forms a valid component
-                } else {
-                    dp[root] = (dp[root] + dp[neighbor]) % k; // Aggregate with modulo
-                }
+    public static void dfs(ArrayList<Integer> list[],int root, int dp[], int values[],int parent,int k,int ct[]){
+
+        for(int u:list[root]){
+            if(u!=parent) dfs(list,u,dp,values,root,k,ct);
+        }
+        dp[root]=values[root];
+        for(int u:list[root]){
+            if(u!=parent){
+                if(dp[u]%k==0) ct[0]++;
+                else dp[root]+=dp[u];
             }
         }
+       // System.out.println(dp[root]);
     }
 }
