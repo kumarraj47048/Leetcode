3b5e1d0edc22f9555130c02a540057924feb2587
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-01-02 01:16:32 +0530

    Time: 469 ms (16.79%) | Memory: 55.2 MB (32.12%) - LeetSync

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String clearStars(String s) {
+        int n=s.length();
+        char ch[]=s.toCharArray();
+        //Pair arr[]=new Pair[n];
+        PriorityQueue<Pair> p=new PriorityQueue<>((a,b)->{
+            if(a.c!=b.c) return Character.compare(a.c,b.c);
+            else return Integer.compare(b.idx,a.idx);
+        }
+        );
+        for(int i=0;i<n;i++){
+            if(ch[i]=='*') p.poll();
+            else p.add(new Pair(ch[i],i));
+        }
+        Pair arr[]=new Pair[p.size()];
+        int i=0;
+        
+        while(p.size()>0){
+            System.out.print(p.peek().c+" "+p.peek().idx+" ,");
+            arr[i++]=new Pair(p.peek().c,p.poll().idx);
+        }
+       
+        Arrays.sort(arr,(a,b)->Integer.compare(a.idx,b.idx));
+        StringBuilder sb=new StringBuilder("");
+        for( i=0;i<arr.length;i++){
+            sb.append(arr[i].c);
+        }
+        return sb.toString();
+    }
+}
+class Pair{
+    Character c;
+    int idx;
+    Pair(Character c, int idx){
+        this.c=c;
+        this.idx=idx;
+    }
+}
