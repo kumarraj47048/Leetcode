5c611350eb36cc01dfe78a1cc1e1a4757a690bad
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-01-07 19:24:05 +0530

    Time: 435 ms (5.03%) | Memory: 45 MB (38.68%) - LeetSync

--- 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,31 +1,21 @@
 class Solution {
     public int maxLength(List<String> arr) {
-       return dfs(arr,"",0);
-          }
-
-    public static int dfs(List<String> arr, String s, int curr){
-        if(curr==arr.size())
-        return s.length();
-      int left=0;
-      int right=0;
-      String temp=s+arr.get(curr);
-      if(isUnique(temp)){
-          left=dfs(arr,temp,curr+1);
-      }
-      right=dfs(arr,s,curr+1);
-
-      return Math.max(left,right);
+        int n=arr.size();
+        int length=Math.max(rec(arr,0,""), rec(arr,0,arr.get(0)));
+        return length;
     }
-
-    public static boolean isUnique(String temp){
-        int ans[]=new int[26];
-        for(int i=0;i<temp.length();i++){
-            ans[temp.charAt(i)-'a']++;
+    private int rec(List<String> arr, int idx,String s){
+        int n=arr.size();
+        if(idx>=n){
+            if(check(s)) return s.length();
+            else return 0;
         }
-        for(int i=0;i<ans.length;i++){
-            if(ans[i]>1)
-            return false;
-        }
+        return Math.max(rec(arr,idx+1,s+(arr.get(idx))),rec(arr,idx+1,s));
+    }
+    private boolean check(String s){
+        int ct[]=new int[26];
+        for(char c:s.toCharArray()) ct[c-'a']++;
+        for(int i=0;i<26;i++) if(ct[i]>1) return false;
         return true;
     }
 }
