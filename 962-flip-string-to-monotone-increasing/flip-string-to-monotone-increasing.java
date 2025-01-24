5467f41a06bfcb0c9b9a97dcc30cebd5ee877bd5
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-01-24 21:25:40 +0530

    Time: 7 ms (95.85%) | Memory: 45.3 MB (76.47%) - LeetSync

--- 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -1,40 +1,17 @@
 class Solution {
     public int minFlipsMonoIncr(String s) {
         char ch[]=s.toCharArray();
-        int n=s.length();
-        int dp[][]=new int[n][2];
-        for(int i=0;i<n;i++){
-            dp[i][0]=-1;
-            dp[i][1]=-1;
-        }
-        dp[n-1][0]=func(0,0,ch,dp);
-        dp[n-1][1]=func(0,1,ch,dp);
-
-        return Math.min(dp[n-1][0], dp[n-1][1]);
-    }
-    public static int func(int i, int op, char ch[], int dp[][]){
         int n=ch.length;
-        if(i==n) return 0;
-        if(ch[i]=='0'){
-            if(op==0){
-                if(dp[i][0]!=-1) return dp[i][0];
-                else dp[i][0]=Math.min(func(i+1,0,ch,dp),func(i+1,1,ch,dp));
+        int ones=0;
+        int flip=0;
+        for(char c:ch){
+            if(c=='0'){
+                flip=Math.min(flip+1,ones);
             }
             else{
-                if(dp[i][1]!=-1) return dp[i][1];
-                else dp[i][1]=func(i+1,1,ch,dp)+1;
+                ones++;
             }
         }
-        else{
-            if(op==0){
-                if(dp[i][0]!=-1) return dp[i][0];
-                else dp[i][0]=Math.min(func(i+1,0,ch,dp),func(i+1,1,ch,dp))+1;
-            }
-            else{
-                if(dp[i][1]!=-1) return dp[i][1];
-                else dp[i][1]=func(i+1,1,ch,dp);
-            }
-        }
-        return dp[i][op];
+        return flip;
     }
 }
