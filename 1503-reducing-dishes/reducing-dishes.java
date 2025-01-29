b1737248cdd79caf7def18013d80f92d4839a77f
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-01-29 15:26:54 +0530

    Time: 2 ms (100.00%) | Memory: 41.7 MB (74.40%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.java
+++ 1503-reducing-dishes/reducing-dishes.java
@@ -1,26 +1,20 @@
+import java.util.Arrays;
+
 class Solution {
     public int maxSatisfaction(int[] satisfaction) {
-        int n=satisfaction.length;
         Arrays.sort(satisfaction);
-        long dp[][]=new long[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                dp[i][j]=Integer.MIN_VALUE;
-            }
-        }
-        // for time equals to zero, sum will be 0
+        int n = satisfaction.length;
+        int maxSum = 0, suffixSum = 0, result = 0;
 
-        for(int i=0;i<=n;i++) dp[i][0]=0;
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                long take=satisfaction[i-1]*j+dp[i-1][j-1];
-                long not_take=dp[i-1][j];
-                dp[i][j]=Math.max(take,not_take);
+        for (int i = n - 1; i >= 0; i--) {
+            suffixSum += satisfaction[i];  // Add current dish to suffix sum
+            if (suffixSum + maxSum > maxSum) {
+                maxSum += suffixSum;  // Update max sum
+                result = maxSum;
+            } else {
+                break;  // Stop if including current dish doesn't improve result
             }
         }
-        long max=0;
-        for(int i=1;i<=n;i++) max=Math.max(max,dp[n][i]);
-        return (int)max;
+        return result;
     }
 }
