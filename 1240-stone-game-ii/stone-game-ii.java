cdcfad1edf44a78c29c147808d850e122a0c3292
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-02-17 11:41:13 +0530

    Time: 25 ms (11.07%) | Memory: 45.7 MB (5.87%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.java
+++ 1240-stone-game-ii/stone-game-ii.java
@@ -1,28 +1,36 @@
 class Solution {
-      private int[] s;
-        private Integer f[][];
-        private int n;
     public int stoneGameII(int[] piles) {
-      n=piles.length;
-      s=new int[n+1];
-      f=new Integer[n][n+1];
-      for(int i=0;i<n;i++){
-          s[i+1]=s[i]+piles[i];
-      }
-      return dfs(0,1);
+        int n=piles.length;
+        int m=1;
+        int maxScore=0;
+        int dp[][][]=new int[n][2*n][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<2*n;j++){
+                dp[i][j][0]=-1;
+                dp[i][j][1]=-1;
+            }
+        }
+        return solve(0,1,1,piles,dp);
     }
-
-    private int dfs(int i, int m){
-        if(m*2>=n-i){
-            return s[n]-s[i];
+    public static int solve(int idx,int m, int op, int piles[], int dp[][][]){
+        if(idx>piles.length-1) return 0;
+        
+        int score=0;
+        if(op%2==0) score=Integer.MAX_VALUE;
+        int currSum=0;
+        if(dp[idx][m][op%2]!=-1) return dp[idx][m][op%2];
+        if(op%2==0){
+            for(int i=idx;i<Math.min(2*m+idx,piles.length);i++){
+                currSum+=piles[i];
+                score=Math.min(score,solve(i+1,Math.max(m,i-idx+1),op+1,piles,dp));
+            }
         }
-        if(f[i][m]!=null){
-            return f[i][m];
+        else{
+            for(int i=idx;i<Math.min(2*m+idx,piles.length);i++){
+                currSum+=piles[i];
+                score=Math.max(score,currSum+solve(i+1,Math.max(m,i-idx+1),op+1,piles,dp));
+            }
         }
-        int res=0;
-        for(int x=1;x<=m*2;x++){
-            res=Math.max(res, s[n]-s[i]-dfs(i+x, Math.max(m,x)));
-        }
-        return f[i][m]=res;
+        return dp[idx][m][op%2]=score;
     }
 }
