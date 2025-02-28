a4d6a5ccf92fe153887fa07ad8d304c841ca95a5
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-02-28 21:06:55 +0530

    Time: 24 ms (39.66%) | Memory: 50.5 MB (49.87%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -1,48 +1,40 @@
 class Solution {
     public String shortestCommonSupersequence(String str1, String str2) {
-        String str=lcs(str1,str2);
-        String ans="";
-        int p1=0,p2=0,p3=0;
-        for(char c:str.toCharArray()){
-            while(p1<str1.length() && str1.charAt(p1)!=c){
-                ans+=str1.charAt(p1);
-                p1++;
-            }
-            while(p2<str2.length() && str2.charAt(p2)!=c){
-                ans+=str2.charAt(p2);
-                p2++;
-            } 
-            ans+=c;
-            p1++;
-            p2++;
-        }
-        if(p1<str1.length()) ans+=str1.substring(p1);
-        if(p2<str2.length()) ans+=str2.substring(p2);
-        return ans;
-    }
-    public static String lcs(String s1, String s2){
-        int m=s1.length();
-        int n=s2.length();
-        int dp[][]=new int[m+1][n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1))
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=Math.max(dp[i-1][j], dp[i][j-1]);
+        int m = str1.length(), n = str2.length();
+        
+        // Step 1: Compute LCS using Bottom-Up DP
+        int dp[][] = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
             }
         }
-        StringBuilder sb=new StringBuilder("");
-        int i=m,j=n;
-        while(i>0 && j>0){
-            if(s1.charAt(i-1)==s2.charAt(j-1)){
-                sb.append(s1.charAt(i-1));
-                i--;
-                j--;
+        
+        // Step 2: Construct SCS using LCS table
+        StringBuilder sb = new StringBuilder();
+        int i = m, j = n;
+        
+        while (i > 0 && j > 0) {
+            if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                sb.append(str1.charAt(i - 1));
+                i--; j--;  // Move diagonally
+            } else if (dp[i - 1][j] > dp[i][j - 1]) {
+                sb.append(str1.charAt(i - 1));
+                i--;  // Move up
+            } else {
+                sb.append(str2.charAt(j - 1));
+                j--;  // Move left
             }
-            else if(dp[i-1][j]>dp[i][j-1]) i--;
-            else j--;
         }
+        
+        // Add remaining characters
+        while (i > 0) sb.append(str1.charAt(--i));
+        while (j > 0) sb.append(str2.charAt(--j));
+        
         return sb.reverse().toString();
     }
 }
