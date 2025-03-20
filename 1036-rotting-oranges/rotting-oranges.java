bafdc97673a9ebaa31cd85d72dc0540c22df67dd
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-03-20 20:23:01 +0530

    Time: 1 ms (100.00%) | Memory: 42.4 MB (75.29%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,66 +1,50 @@
-class Solution
-{
-    //Function to find minimum time required to rot all oranges. 
-    public int orangesRotting(int[][] grid)
-    {
-        // Code here
+class Solution {
+    public int orangesRotting(int[][] grid) {
         int row=grid.length;
         int col=grid[0].length;
-        Queue<Pair> q=new LinkedList<>();
+        Queue<int[]> q=new LinkedList<>();
+        int t=0;
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==2){
-                    q.add(new Pair(i,j));
-                    grid[i][j]=0;
+                    q.add(new int[]{i,j});
                 }
             }
         }
         
-        int t=0;
-        int size=q.size();
         while(q.size()>0){
-            Pair curr=q.poll();
-            int x=curr.x;
-            int y=curr.y;
-            if(x-1>=0 && grid[x-1][y]==1){
-                q.add(new Pair(x-1,y));
-                grid[x-1][y]=0;
+            int sz=q.size();
+            for(int i=0;i<sz;i++){
+                int curr[]=q.poll();
+                int x=curr[0];
+                int y=curr[1];
+
+                if(x-1>=0 && grid[x-1][y]==1){
+                   grid[x-1][y]=2;
+                   q.add(new int[]{x-1,y});
+                }
+                if(x+1<row && grid[x+1][y]==1){
+                   grid[x+1][y]=2;
+                   q.add(new int[]{x+1,y});
+                }
+                if(y-1>=0 && grid[x][y-1]==1){
+                   grid[x][y-1]=2;
+                   q.add(new int[]{x,y-1});
+                }
+                if(y+1<col && grid[x][y+1]==1){
+                   grid[x][y+1]=2;
+                   q.add(new int[]{x,y+1});
+                }
+ 
             }
-            if(y-1>=0 && grid[x][y-1]==1){
-                q.add(new Pair(x,y-1));
-                grid[x][y-1]=0;
-            }
-            if(x+1<row && grid[x+1][y]==1){
-                q.add(new Pair(x+1,y));
-                grid[x+1][y]=0;
-            }
-            if(y+1<col && grid[x][y+1]==1){
-                q.add(new Pair(x,y+1));
-                grid[x][y+1]=0;
-            }
-            size--;
-            if(size==0){
-                size=q.size();
-                t++;
-            }
+            t++;
         }
-        
-      
-        
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(grid[i][j]==1) return -1;
             }
         }
-          if(t==0) return t;
+        if(t==0) return t;
         return t-1;
-        
-    }
-}
-class Pair{
-    int x,y;
-    Pair(int x,int y){
-        this.x=x;
-        this.y=y;
     }
 }
