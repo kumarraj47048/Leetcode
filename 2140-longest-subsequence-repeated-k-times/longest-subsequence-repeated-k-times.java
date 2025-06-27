0bf2417898bcfea6807334fe51f30625c95b411f
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-06-28 01:08:03 +0530

    Time: 244 ms (33.33%) | Memory: 45.8 MB (10.53%) - LeetSync

---
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String longestSubsequenceRepeatedK(String s, int k) {
+        char ch[]=s.toCharArray();
+        int n=ch.length;
+        Queue<String> q=new LinkedList<>();
+        int ct[]=new int[26];
+        for(char c:ch){
+            ct[c-'a']++;
+        }
+
+        String ans="";
+        for(int i=0;i<26;i++){
+            if(ct[i]>=k){
+                String x=Character.toString((char)(i+'a'));
+                q.add(x);
+                ans=ans.compareTo(x)>=0?ans:x;
+            }
+        }
+
+        while(q.size()>0){
+            String curr=q.poll();
+            for(int i=0;i<26;i++){
+                String temp=curr+Character.toString((char)(i+'a'));
+                if(check(ch,k,temp)){
+                    if(ans.length()<temp.length()) ans=temp;
+                    else if(ans.length()==temp.length())
+                    ans=ans.compareTo(temp)>=0?ans:temp;
+                    q.add(temp);
+                }
+            }
+        }
+
+        return ans;
+    }
+    public static boolean check(char ch[], int k, String part){
+        int n=ch.length;
+        int m=part.length();
+        char pt[]=part.toCharArray();
+        int ct=0;
+        int p1=0;
+        for(char c:ch){
+            if(pt[p1]==c){
+                p1++;
+                if(p1>=m){
+                    ct++;
+                    p1=0;
+                }
+            }
+        }
+
+        return ct>=k;
+    }
+}
