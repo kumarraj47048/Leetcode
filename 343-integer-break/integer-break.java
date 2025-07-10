43947a35d0b3f6f46dca6ef5bc333c193417b621
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-07-11 00:24:48 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (67.29%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,20 +1,17 @@
 class Solution {
-    int max=1;
+   // int max=1;
     public int integerBreak(int n) {
-        if(n==2) return 1;
-        if(n==3) return 2;
-        int dp[]=new int[60];
-        Arrays.fill(dp,-1);
+       int dp[]=new int[60];
+       Arrays.fill(dp,-1);
         return solve(n,dp);
     }
     public int solve(int n, int dp[]){
-        
-        if(n==0) return 1;
+        if(n==1) return 1;
         if(dp[n]!=-1) return dp[n];
         int temp=1;
-        for(int i=1;i<=n;i++){
-            if(n-i>=0)
-            temp=Math.max(temp, (i)*(solve(n-i,dp)));
+        for(int i=1;i<=n-1;i++){
+            int x=i*(Math.max(n-i,solve(n-i,dp)));
+            temp=Math.max(temp, x);
         }
         return dp[n]=temp;
     }
