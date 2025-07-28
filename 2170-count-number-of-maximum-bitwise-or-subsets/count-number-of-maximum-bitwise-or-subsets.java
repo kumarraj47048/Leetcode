aa1823581efcd136afbd4dc8c6398c9417433460
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-07-28 22:51:06 +0530

    Time: 13 ms (33.05%) | Memory: 44.8 MB (22.81%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.java
@@ -3,14 +3,22 @@ class Solution {
         int n=nums.length;
         int max=0;
         for(int i:nums) max=max|i;
-        return solve(0,0,nums,max);
+        //int dp[]=new int[n];
+        HashMap<String,Integer> map=new HashMap<>();
+        //Arrays.fill(dp,-1);
+        return solve(0,0,nums,max,map);
     }
-    public int solve(int idx,int curr, int nums[], int max){
+    public int solve(int idx,int curr, int nums[], int max, HashMap<String,Integer> map){
         int n=nums.length;
         if(idx>=n){
             if(curr==max) return 1;
             else return 0;
         }
-        return solve(idx+1,curr|nums[idx],nums,max) + solve(idx+1,curr,nums,max);
+        String key=idx+" "+curr;
+        if(map.containsKey(key)) return map.get(key);
+       // if(dp[idx]!=-1) return dp[idx];
+        int x=solve(idx+1,curr|nums[idx],nums,max,map) + solve(idx+1,curr,nums,max,map);
+        map.put(key,x);
+        return x;
     }
 }
