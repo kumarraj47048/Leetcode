a85fb0b243de608bf279d8373a7fe78379329b4b
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-08-01 12:53:50 +0530

    Time: 1034 ms (5.18%) | Memory: 110.6 MB (5.00%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.java
+++ 2808-painting-the-walls/painting-the-walls.java
@@ -1,22 +1,23 @@
 class Solution {
     public int paintWalls(int[] cost, int[] time) {
-        int n = cost.length;
-        Integer[][] dp = new Integer[n + 1][n + 1];
-        return dfs(0, n, cost, time, dp);
+        int n=cost.length;
+        HashMap<String,Integer> map=new HashMap<>();
+        return solve(0,cost.length,cost,time,map);
     }
+    public static int solve(int idx, int left, int cost[], int time[], HashMap<String,Integer> map){
+        int n=cost.length;
+        if(left<0) left=0;
+        if(idx>=n){
+            if(left<=0) return 0;
+            else return Integer.MAX_VALUE/2;
+        }
+        String key=idx+","+left;
+        if(map.containsKey(key)) return map.get(key);
+        int pick=cost[idx]+solve(idx+1,left-time[idx]-1,cost,time,map);
+        int not_pick=solve(idx+1,left,cost,time,map);
+        int val=Math.min(pick,not_pick);
+        map.put(key,val);
+        return val;
 
-    private int dfs(int i, int wallsLeft, int[] cost, int[] time, Integer[][] dp) {
-        if (wallsLeft <= 0) return 0;
-        if (i == cost.length) return Integer.MAX_VALUE / 2; // avoid overflow
-
-        if (dp[i][wallsLeft] != null) return dp[i][wallsLeft];
-
-        // Option 1: Paint current wall yourself -> reduce wallsLeft by 1 + time[i]
-        int pick = cost[i] + dfs(i + 1, Math.max(0, wallsLeft - 1 - time[i]), cost, time, dp);
-
-        // Option 2: Skip current wall
-        int skip = dfs(i + 1, wallsLeft, cost, time, dp);
-
-        return dp[i][wallsLeft] = Math.min(pick, skip);
     }
 }
