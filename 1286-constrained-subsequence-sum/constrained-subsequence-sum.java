baab234aaa16bef16d4da149981afead993fda45
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-08-02 16:38:38 +0530

    Time: 91 ms (16.54%) | Memory: 57.2 MB (62.60%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.java
@@ -3,10 +3,6 @@ class Solution {
         int n=nums.length;
         int dp[]=new int[n];
         int result=Integer.MIN_VALUE;
-        // for(int i=0;i<n;i++){
-        //     dp[i]=nums[i];
-        //     result=Math.max(result,dp[i]);
-        // }
         PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[0]-a[0]);
         pq.add(new int[]{dp[0],0});
         for(int i=0;i<n;i++){
