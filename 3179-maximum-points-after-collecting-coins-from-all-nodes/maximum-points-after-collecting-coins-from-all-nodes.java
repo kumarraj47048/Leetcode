98697257e6c5bb7ffe5715bbc25164d4d5fecb7a
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-08-04 02:13:58 +0530

    Time: 821 ms (8.77%) | Memory: 316.6 MB (5.26%) - LeetSync

---
+++ 3179-maximum-points-after-collecting-coins-from-all-nodes/maximum-points-after-collecting-coins-from-all-nodes.java
@@ -0,0 +1,41 @@
+class Solution {
+    public static int k;
+    public int maximumPoints(int[][] edges, int[] coins, int K) {
+        k=K;
+        int n=coins.length;
+        if(k==0){
+            int sum=0;
+            for(int i:coins) sum+=i;
+            return sum;
+        }
+        ArrayList<Integer> tree[]=new ArrayList[n];
+        for(int i=0;i<n;i++) tree[i]=new ArrayList<>();
+        for(int i[]:edges){
+            tree[i[0]].add(i[1]);
+            tree[i[1]].add(i[0]);
+        }
+        HashMap<String,Integer> map=new HashMap<>();
+
+        return dfs(0,-1,0,tree,coins,map);
+
+    }
+    public static int dfs(int root, int par, int p, ArrayList<Integer> tree[], int coins[], HashMap<String,Integer> map){
+       
+       int a=(coins[root]/(1<<p))-k;
+       int b=coins[root]/(1<<(p+1));
+       String key=root+","+par+","+p;
+       if(map.containsKey(key)) return map.get(key);
+       map.put(key,0);
+       if(p>14) return 0;
+      
+        for(int u:tree[root]){
+            if(u!=par){
+                a+=dfs(u,root,p,tree, coins,map);
+                b+=dfs(u,root,p+1,tree,coins,map);
+            }
+        }
+        int val=Math.max(a,b);
+        map.put(key,val);
+        return val;
+    }
+}
