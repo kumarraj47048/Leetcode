3888e85a729a733ece1f904471d77d84dd4413ca
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-08-04 12:05:00 +0530

    Time: 929 ms (5.05%) | Memory: 59.1 MB (5.05%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -1,36 +1,40 @@
 class Solution {
+    public static int dir[][]={{-2,-2,-1,1,2,2,1,-1},
+                        {-1,1,2,2,1,-1,-2,-2}};
+    public static int mod=1000000007;
     public int knightDialer(int n) {
-        int jumps[][]={
-            {4,6},
-            {6,8},
-            {7,9},
-            {4,8},
-            {3,9,0},
-            {},
-            {1,7,0},
-            {2,6},
-            {1,3},
-            {2,4}
-        };
-
-        int mod=(int) 1e9+7;
-        int dp[][]=new int[n][10];
-        for(int square=0;square<10;square++) dp[0][square]=1;
-
-        for(int remain=1;remain<n;remain++){
-            for(int square=0;square<10;square++){
-                int ans=0;
-                for(int nextSquare: jumps[square]){
-                    ans=(ans+ dp[remain-1][nextSquare])%mod;
+        boolean arr[][]=new boolean[4][3];
+        for(int i=0;i<4;i++) Arrays.fill(arr[i],true);
+        arr[3][0]=false;
+        arr[3][2]=false;
+        int ct=0;
+        HashMap<String,Integer> map=new HashMap<>();
+        for(int i=0;i<4;i++){
+            for(int j=0;j<3;j++){
+                if(arr[i][j]){
+                    ct=(ct+solve(n,i,j,arr,map))%mod;
                 }
-                dp[remain][square]=ans;
             }
         }
 
-        int ans=0;
-        for(int square=0;square<10;square++){
-            ans=(ans+ dp[n-1][square])%mod;
+        return ct;
+    }
+
+    public static int solve(int steps, int i, int j, boolean arr[][], HashMap<String,Integer> map){
+
+        int ct=0;
+        if(steps==1) return 1;
+        String key=steps+","+i+","+j;
+        if(map.containsKey(key)) return map.get(key);
+        for(int m=0;m<8;m++){
+            int new_row=i+dir[0][m];
+            int new_col=j+dir[1][m];
+
+            if(new_row>=0 && new_row<4 && new_col>=0 && new_col<3 && arr[new_row][new_col]){
+                ct=(ct+solve(steps-1,new_row,new_col,arr,map))%mod;
+            }
         }
-        return ans;
+        map.put(key,ct);
+        return ct;
     }
 }
