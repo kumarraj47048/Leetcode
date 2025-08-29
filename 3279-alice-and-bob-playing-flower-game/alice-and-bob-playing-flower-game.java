6516f6dd6975ba5ed5a5446178f561e6e36a7d0f
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-08-29 23:23:14 +0530

    Time: 2 ms (7.50%) | Memory: 40.6 MB (86.88%) - LeetSync

---
+++ 3279-alice-and-bob-playing-flower-game/alice-and-bob-playing-flower-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long flowerGame(int n, int m) {
+        long ans=0;
+        for(int i=1;i<=n;i++){
+            if(i%2==0){
+                ans+=m/2;
+                ans+=m%2;
+            }
+            else ans+=m/2;
+        }
+        return ans;
+    }
+}
