1f01fdfbd2caab48d374b89b25485db29db57bc9
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-09-01 08:36:43 +0530

    Time: 60 ms (14.58%) | Memory: 48.5 MB (5.08%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.java
+++ 526-beautiful-arrangement/beautiful-arrangement.java
@@ -1,9 +1,16 @@
 class Solution {
+   // int dp[];
+   HashMap<String,Integer> map=new HashMap<>();
     public int countArrangement(int n) {
+       // dp=new int[n+1];
+        //Arrays.fill(dp,-1);
         return solve(1,new HashSet<Integer>(), n);
     }
     public int solve(int idx, HashSet<Integer> set, int n){
         if(idx==n+1) return 1;
+        String key=idx+","+set;
+        if(map.containsKey(key)) return map.get(key);
+        //if(dp[idx]!=-1) return dp[idx];
         int ans=0;
         for(int i=1;i<=n;i++){
             if(!set.contains(i)){
@@ -14,6 +21,7 @@ class Solution {
                 }
             }
         }
+        map.put(key,ans);
         return ans;
     }
 }
