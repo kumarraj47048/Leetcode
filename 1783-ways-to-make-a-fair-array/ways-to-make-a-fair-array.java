91555d63c3313d7e9f48107da1b06e34a987ac1b
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-09-02 14:43:44 +0530

    Time: 2821 ms (5.48%) | Memory: 57.3 MB (64.26%) - LeetSync

---
+++ 1783-ways-to-make-a-fair-array/ways-to-make-a-fair-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int waysToMakeFair(int[] nums) {
+        int n=nums.length;
+        int odd[]=new int[n+2];
+        int even[]=new int[n+2];
+
+        for(int i=n-1;i>=0;i--){
+            odd[i]=odd[i+1];
+            even[i]=even[i+1];
+            if(i%2==0) odd[i]+=nums[i];
+            else even[i]+=nums[i];
+        }
+        
+        for(int i=0;i<n;i++){
+            System.out.print(even[i]+" ");
+        }
+         
+         System.out.println();
+         for(int i=0;i<n;i++){
+            System.out.print(odd[i]+" ");
+        }
+           
+
+        int ct=0;
+        int x=0;
+        int y=0;
+        for(int i=0;i<n;i++){
+            if(even[i+1]+x==odd[i+1]+y) ct++;
+            if(i%2==0) x+=nums[i];
+            else y+=nums[i];
+        }
+        return ct;
+    }
+}
