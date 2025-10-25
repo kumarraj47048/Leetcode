a6228ff9d75b073a92825e470138d1be4c4f857a
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-10-25 22:34:21 +0530

    Time: 881 ms (5.13%) | Memory: 79 MB (11.74%) - LeetSync

--- 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
+++ 2334-number-of-flowers-in-full-bloom/number-of-flowers-in-full-bloom.java
@@ -1,49 +1,55 @@
-import java.util.*;
-
 class Solution {
     public int[] fullBloomFlowers(int[][] flowers, int[] people) {
-        int m = flowers.length;
-        int n = people.length;
+        int m=flowers.length;
+        int n=people.length;
+        
+        int pf[][]=new int[2*m][2];
+        for(int i=0;i<m;i++){
+            int st=flowers[i][0];
+            int end=flowers[i][1];
 
-        int[][] pf = new int[2 * m][2];
-        for (int i = 0; i < m; i++) {
-            int st = flowers[i][0];
-            int end = flowers[i][1];
+            pf[2*i][0]=st;
+            pf[2*i][1]++;
+            pf[2*i+1][0]=end+1;
+            pf[2*i+1][1]--;
 
-            pf[2 * i][0] = st;
-            pf[2 * i][1] +=1;
-            pf[2 * i + 1][0] = end + 1;
-            pf[2 * i + 1][1] += -1;
         }
 
-        Arrays.sort(pf, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(pf,(a,b)->(a[0]-b[0]));
+         System.out.println(pf[0][0]+"->"+pf[0][1]);
+        for(int i=1;i<2*m;i++){
+            pf[i][1]+=pf[i-1][1];
 
-        for (int i = 1; i < 2 * m; i++) {
-            pf[i][1] += pf[i - 1][1];
+            System.out.println(pf[i][0]+"->"+pf[i][1]);
+            
         }
 
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            int k = people[i];
-            ans[i] = binarySearch(pf, k);
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            int k=people[i];
+            ans[i]=binarySearch(pf,k);
         }
 
-        return ans;
+    return ans;
     }
+    public int binarySearch(int pf[][], int k){
+        int sz=pf.length;
+        int l=0;
+        int r=sz-1;
+        int ans=-1;
+        while(l<=r){
+            int mid=(l+r)/2;
 
-    public int binarySearch(int[][] pf, int k) {
-        int l = 0, r = pf.length - 1;
-        int ans = -1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (pf[mid][0] <= k) {
-                ans = mid;
-                l = mid + 1;
-            } else {
-                r = mid - 1;
+            if(pf[mid][0]<=k){
+                ans=mid;
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
             }
         }
         if(ans==-1) return 0;
+        System.out.println(pf[ans][0]);
         return pf[ans][1];
     }
 }
