74919ecacd1ce712b381eea9933374eefae69731
kumarraj47048 <98460838+kumarraj47048@users.noreply.github.com>
2025-12-09 21:57:55 +0530

    Time: 261 ms (30.23%) | Memory: 185.8 MB (47.29%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.java
+++ 3885-count-special-triplets/count-special-triplets.java
@@ -1,28 +1,34 @@
 class Solution {
     public int specialTriplets(int[] nums) {
-        int n = nums.length;
-        HashMap<Integer, Integer> right = new HashMap<>();
-        HashMap<Integer, Integer> left = new HashMap<>();
-        int mod = (int)1e9 + 7;
-        
-        for (int x : nums)
-            right.put(x, right.getOrDefault(x, 0) + 1);
-
-        long ct = 0;
-
-        for (int i = 0; i < n; i++) {
-            int x = nums[i];
-            
-            right.put(x, right.get(x) - 1); // remove current from right
-
-            int L = left.getOrDefault(x * 2, 0);
-            int R = right.getOrDefault(x * 2, 0);
-
-            ct = (ct + (long)L * R) % mod;
-
-            left.put(x, left.getOrDefault(x, 0) + 1);
+        int n=nums.length;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int mod=(int)1e9+7;
+        for(int i=n-1;i>=0;i--){
+            int x=nums[i];
+            map.put(x,map.getOrDefault(x,0)+1);
         }
 
-        return (int)ct;
+        int ct=0;
+        HashMap<Integer,Integer> map2=new HashMap<>();
+        for(int i=0;i<n;i++){
+            int x=nums[i];
+            
+            map.put(x,map.getOrDefault(x,0)-1);
+
+            int left=map.getOrDefault(x*2,0);
+            int right=map2.getOrDefault(x*2,0);
+            
+            long y=((long)left*right)%mod;
+            
+            //if(i>0 && i<n-1)
+            ct=(ct+(int)y)%mod;
+
+          //  map.put(x,map.get(x)-1);
+            map2.put(x,map2.getOrDefault(x,0)+1);
+            
+        }
+
+
+        return ct;
     }
 }
